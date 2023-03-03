b6c05bb94a33a74b592b9d554afcdc4d4ab531da
ruthjelimo <ruthjmimo@gmail.com>
2023-03-03 10:01:10 +0300

    updated files

--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
-
+import datetime
 from .models import * 
 
 def store(request):
@@ -76,3 +76,17 @@ def updateItem(request):
 		orderItem.delete()
 
 	return JsonResponse('Item was added', safe=False)
+
+def processOrder(request):
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		total = float(data['form']['total'])
+		order.transaction_id = transaction_id
+	else:
+		print('User is not logged in')
+
+	return JsonResponse('Payment submitted..', safe=False)
