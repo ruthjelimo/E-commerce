a718a3848f0e16756b383ee197057a12db9cb995
ruthjelimo <ruthjmimo@gmail.com>
2023-03-07 12:52:01 +0300

    updated files

--- store/utils.py
+++ store/utils.py
@@ -43,4 +43,16 @@ def cookieCart(request):
 
 
 def cartData(request):
-    return {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
+        order = cookieData['order']
+        items = cookieData['items']
+
+    return {'cartItems': cartItems, 'order': order, 'items': items}
