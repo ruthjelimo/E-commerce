8a34fd7edef695416ef213f4d0ac7bf38dd7609c
ruthjelimo <ruthjmimo@gmail.com>
2023-03-07 11:09:07 +0300

    updated files

--- store/views.py
+++ store/views.py
@@ -30,6 +30,12 @@ def cart(request):
 		cartItems = order.get_cart_items
 	else:
 		#Create empty cart for now for non-logged in user
+		try:
+			cart = json.loads(request.COOKIES['cart'])
+		except:
+			cart = {}
+			print('CART:', cart)
+
 		items = []
 		order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
 		cartItems = order['get_cart_items']
@@ -86,6 +92,20 @@ def processOrder(request):
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
 		total = float(data['form']['total'])
 		order.transaction_id = transaction_id
+
+		if total == order.get_cart_total:
+			order.complete = True
+		order.save()
+
+		if order.shipping == True:
+			ShippingAddress.objects.create(
+			customer=customer,
+			order=order,
+			address=data['shipping']['address'],
+			city=data['shipping']['city'],
+			state=data['shipping']['state'],
+			zipcode=data['shipping']['zipcode'],
+			)
 	else:
 		print('User is not logged in')
 
